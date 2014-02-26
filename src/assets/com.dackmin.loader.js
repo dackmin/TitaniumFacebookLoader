ed2b874823ed8d89e327dcda8ecdea298f2071c5
Ugo Stephant <contact@dackmin.com>
2014-02-26 11:52:08 +0100

    Create module base

---
+++ src/assets/com.dackmin.loader.js
@@ -0,0 +1,12 @@
+function FacebookLoader(){
+	this.top = 0;
+	this.left = 0;
+	this.right = 0;
+	this.bottom = 0;
+};
+
+FacebookLoader.prototype.createLoader = function(options){
+	
+};
+
+exports.FacebookLoader = FacebookLoader;
